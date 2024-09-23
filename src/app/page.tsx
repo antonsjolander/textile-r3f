76ce9395a8bb3ae5df2cd77f006040310d2bc229
Anton Strandqvist <anton.strandqvist@umain.com>
2024-09-23 11:45:59 +0200

    added scroll lable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
       <div className="w-full h-svh"></div>
       <div className="w-full h-svh"></div>
       <div className="w-full h-svh"></div>
+      <div className="fixed left-0 bottom-4 text-center w-full">
+        scroll down
+      </div>
     </div>
   );
 }
